1004b068a88952cdf0e5ec05f0332c42f710a2c0
wlm.morant <wlm.morant@gmail.com>
2021-01-23 00:07:14 -0500

    classList.toggle

--- main.js
+++ main.js
@@ -2,5 +2,6 @@ const btnToggle = document.querySelector('.toggle-btn')
 console.log(btnToggle)
 
 btnToggle.addEventListener('click',function(){
-    console.log('clicked')
+    //console.log('clicked')
+    document.getElementById('sidebar').classList.toggle('active')
 })
